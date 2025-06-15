302357376a72ef62fb7dbea6a694b82408f9f2ce
Nerevakiin <102064286+Nerevakiin@users.noreply.github.com>
2025-06-15 20:45:04 +0300

    added all files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+
+let saveEl = document.getElementById("save-el")
+
+
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+    console.log(count)
+}
+
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+    console.log(count)
+}
+
+function save() {
+    
+    let saves = count + " - " 
+    saveEl.textContent = saveEl.textContent + saves
+    countEl.textContent = 0 
+    count = 0
+    
+    
+}
+
+save()
